16125eb5b17ea08abd85c30d8dff45671853532a
Seawis <sergio55@yandex.ru>
2025-06-24 10:27:40 +0600

    index

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 import { compareObjects } from './src/compare.js'
+import getObject from './src/parsers.js'
 
-export default compareObjects
+const gendiff = (filepath1, filepath2, f) => compareObjects(getObject(filepath1), getObject(filepath2), f)
+
+export default gendiff
